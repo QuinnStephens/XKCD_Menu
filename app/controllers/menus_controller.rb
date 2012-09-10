39ccd2e272ddaebdb0761d9acc0ca7a4aff0c898
Quinn Stephens <quinn@quinnstephens.com>
2012-09-10 09:59:17 -0500

    Cleaned up some extra code

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,7 +1,7 @@
 class MenusController < ApplicationController
 
   def index
-    @menus = Menu.all
+    redirect_to [:new, :menu]
   end
 
   def show
@@ -13,28 +13,12 @@ class MenusController < ApplicationController
     @menu = Menu.new
   end
 
-  def edit
-    @menu = Menu.find(params[:id])
-  end
-
   def create
     @menu = Menu.new(params[:menu])
 
     respond_to do |format|
       if @menu.save
         format.html { redirect_to @menu, {"notice" => "We have processed your menu problem!"}}
-      else
-        format.html { render action: "new"}
-      end
-    end
-  end
-
-  def update
-    @menu = Menu.find(params[:id])
-
-    respond_to do |format|
-      if @menu.update_attributes(params[:menu])
-        format.html { redirect_to @menu, {"notice" => "Menu was successfully updated."}}
       else
         format.html { render action: "new"}
       end
